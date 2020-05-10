3def49a2a03f1740be9c462aaca4cc02d17728ae
Marwin Haerttrich <marwin.haerttrich@gmail.com>
2020-05-10 22:26:56 +0200

    function visitor

--- usercode_parser/parser.py
+++ usercode_parser/parser.py
@@ -26,7 +26,40 @@ def find_module(name, tree):
         return True
     return False
 
+class NamedArg:
+    # stores name and line of named arguments
+    # line is needed for possible future output of error message
+    def __init__(self, name, line):
+        self.name = name
+        self.line = line
 
+    def get_name(self):
+        return self.name
+
+    def get_line(self):
+        return self.line
+
+class FunctionVisitor(ast.NodeVisitor):
+    # gets all function calls and finds function name, named args, and code line
+    # missing: package-path, real name, check for relevancy of function
+    def visit_Call(self, node: ast.Call) -> Any:
+        name = self.get_name(node)
+        keywords = self.get_keywords(node)
+        line = node.lineno
+        path = ''
+        # have to findout how to get the package path
+        print('FUNCTION CALL::: in line ', line, ' of function ', name, ' with named args ', keywords)
+
+    def get_name(self, node):
+        name = node.func.value.id
+        name += '.' + node.func.attr
+        return name
+
+    def get_keywords(self, node):
+        list = []
+        for keyword in node.keywords:
+            list.append(keyword.arg)
+        return list
 
 
 if __name__ == '__main__':
@@ -39,8 +72,13 @@ if __name__ == '__main__':
     contents = file.read()
 
     tree = ast.parse(contents)
+    #print(ast.dump(tree))
 
-    if find_module('torch', tree):
+    """
+    if find_module('numpy', tree):
         print('pytorch wurde in der Datei importiert.')
     else:
         print('pytorch wurde in der Datei nicht importiert.')
+    """
+
+    FunctionVisitor().visit(tree)
