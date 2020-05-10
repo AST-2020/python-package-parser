30a69a59f79def281491474aff25eea0158427b8
arnaout <s6gharno@uni-bonn.de>
2020-05-11 00:19:55 +0200

    G2_pytorch folder added

---
+++ G2_pytorch/dict/pt_test.py
@@ -0,0 +1,17 @@
+# from torch import *
+
+class K:
+    def __init__(self):
+        pass
+
+    def f(self):
+        pass
+
+def h():
+    pass
+
+
+k = K()
+print( type( k.f ))
+print( type(k) )
+print( type( h ))
