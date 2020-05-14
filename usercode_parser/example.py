cb7267d7e00cafcb7b6e4e8e6eed8d5031355af0
Marwin Haerttrich <marwin.haerttrich@gmail.com>
2020-05-15 01:08:59 +0200

    bug fixes

--- usercode_parser/example.py
+++ usercode_parser/example.py
@@ -1,15 +1,15 @@
-import torch as t
-import numpy as np
+import torch
 
 
-dtype = t.float
-dev = t.device("cpu")
-# device = torch.device("cuda:0") # Uncomment this to run on GPU
+if __name__=='__main__':
+    dtype = torch.float
+    dev = torch.device("cpu")
+    # device = torch.device("cuda:0") # Uncomment this to run on GPU
 
-# N is batch size; D_in is input dimension;
-# H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 64, 1000, 100, 10
+    # N is batch size; D_in is input dimension;
+    # H is hidden dimension; D_out is output dimension.
+    N, D_in, H, D_out = 64, 1000, 100, 10
 
-# Create random input and output data
-x = t.randn(N, D_in, device=dev, dtype=dtype)
-y = t.randn(N, D_out, device=dev, dtype=dtype)
+    # Create random input and output data
+    x = torch.randn(N, D_in, device=dev, dtype=dtype)
+    y = torch.randn(N, D_out, device=dev, dtype=dtype)
