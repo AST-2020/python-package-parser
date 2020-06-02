f2b6327b6e5b8ae5dad013754f2ee933a24649ae
hady1996 <hadyelkady1996@gmail.com>
2020-06-02 14:35:20 +0200

    bug fixes (zweite version) enthalten ist eine (how_to_use), wo es ist gezeigt, wie man Data vom Struktur erhalten kann

--- library/library_model.py
+++ library/library_model.py
@@ -9,6 +9,13 @@ from typing import List, Any, Dict, TypeVar, Optional
 # It will provide the same output as before, and don’t forget to remove the ‘()’ after func when printing it.
 # Just writing @property above the function any_func() make it available to be used as a property.
 
+def convert_parameter_to_python(parameter: Dict):
+    return Parameter(parameter["_Parameter__name"],
+                     parameter["_Parameter__has_default"],
+                     parameter["_Parameter__default"]
+                     )
+
+
 class Parameter:
     def __init__(self, name: str, has_default: bool = False, default: Any = None):
         self.__name = name
@@ -31,15 +38,26 @@ class Parameter:
             return self.__name
 
 
+def convert_function_to_python(function: Dict):
+    parameters: Dict = function["_Function__parameters"]
+    parameter_names = parameters.keys()
+
+    for parameter_name in parameter_names:
+        parameters[parameter_name] = convert_parameter_to_python(parameters[parameter_name])
+
+    return Function(function["_Function__name"],
+                    parameters.values())
+
+
 class Function:
     def __init__(self, name: str, parameters: List[Parameter]):
         self.__name = name
-        self.__parameters = _list_to_dict(parameters)
+        self.__parameters: Dict[str: Parameter] = _list_to_dict(parameters)
 
     def get_name(self) -> str:
         return self.__name
 
-    def get_parameter(self, parameter_name: str) -> Optional[Parameter]:
+    def get_parameter(self, parameter_name: str) -> Parameter:
         return self.__parameters[parameter_name]
 
     def get_parameters(self) -> List[Parameter]:
@@ -50,11 +68,22 @@ class Function:
         return f"def {self.__name}({parameter_string})"
 
 
+def convert_class_to_python(klass: Dict):
+    methods: Dict = klass["_Class__methods"]
+    method_names = methods.keys()
+
+    for method_name in method_names:
+        methods[method_name] = convert_function_to_python(methods[method_name])
+    return Class(klass["_Class__name"],
+                 methods.values(),
+                 klass["_Class__to_ignore"])
+
+
 class Class:
-    def __init__(self, name: str, methods: List[Function]):
+    def __init__(self, name: str, methods: List[Function], to_ignore=[]):
         self.__name = name
-        self.__methods = _list_to_dict(methods)
-        self.__to_ignore = []
+        self.__methods: Dict[str: Function] = _list_to_dict(methods)
+        self.__to_ignore = to_ignore
 
     def add_to_ignore(self, method_name):
         self.__to_ignore.append(method_name)
@@ -77,12 +106,30 @@ class Class:
         return _dict_to_list(self.__methods)
 
 
+def convert_module_to_python(module: Dict):
+    functions: Dict = module["_Module__top_level_functions"]
+    function_names = functions.keys()
+
+    for fucntion_name in function_names:
+        functions[fucntion_name] = convert_function_to_python(functions[fucntion_name])
+
+    classes: Dict = module["_Module__classes"]
+    class_names = classes.keys()
+
+    for class_name in class_names:
+        classes[class_name] = convert_class_to_python(classes[class_name])
+
+    return Module(module["_Module__name"],
+                  classes.values(),
+                  functions.values(),
+                  module["_Module__to_ignore"])
+
 class Module:
-    def __init__(self, name: str, classes: List[Class], top_level_functions: List[Function]):
+    def __init__(self, name: str, classes: List[Class], top_level_functions: List[Function], to_ignore=[]):
         self.__name = name
-        self.__classes = _list_to_dict(classes)
-        self.__top_level_functions = _list_to_dict(top_level_functions)
-        self.__to_ignore = []
+        self.__classes: Dict[str: Class] = _list_to_dict(classes)
+        self.__top_level_functions: Dict[str: Function] = _list_to_dict(top_level_functions)
+        self.__to_ignore = to_ignore
 
     def add_to_ignore(self, func_name):
         self.__to_ignore.append(func_name)
@@ -110,7 +157,7 @@ class Module:
                 self.add_to_ignore(function.get_name())
 
     def get_top_level_function(self, function_name: str) -> Optional[Function]:
-        return self.__top_level_functions[function_name]
+        return self.__top_level_functions.get(function_name)
 
     def get_top_level_functions(self) -> List[Function]:
         return _dict_to_list(self.__top_level_functions)
@@ -118,7 +165,7 @@ class Module:
 
 class Library:
     def __init__(self, modules: List[Module]):
-        self.__modules = _list_to_dict(modules)
+        self.__modules: Dict[str:Module] = _list_to_dict(modules)
         self.__to_ignore = []
 
     def add_to_ignore(self, func_name):
@@ -130,7 +177,7 @@ class Library:
     def add_module(self, module: Module):
         _add_module_to_dict(self.__modules, module)
 
-    def get_module(self, module_path: str) -> Optional[Module]:
+    def get_module(self, module_path: str) -> Module:
         return self.__modules[module_path]
 
     def get_modules(self) -> List[Module]:
@@ -149,14 +196,12 @@ class Library:
         klass = self.get_class(module_path, class_name)
         if klass is None:
             return None
-
         return klass.get_method(method_name)
 
-    def get_top_level_function(self, module_path: str, function_name: str) -> Optional[Function]:
+    def get_top_level_function(self, module_path: str, function_name: str) -> Function:
         module = self.get_module(module_path)
         if module is None:
             return None
-
         return module.get_top_level_function(function_name)
 
     # convert data into Json format
@@ -168,18 +213,22 @@ class Library:
             json.dump(json_object, outfile)
         print("Package " + package_name + " has been successfully parsed")
 
-    # ex data_file_path = 'testTextFile.txt'
+    # ex: data_file_path = 'testTextFile.txt'
     def convert_to_python(self, data_file_path):
         with open(data_file_path) as json_file:
             python_data = json.load(json_file)
-        self.__modules = json.loads(python_data)
+        modules: Dict = json.loads(python_data)
+        module_names = modules.keys()
+        for module_name in module_names:
+            modules[module_name] = convert_module_to_python(modules[module_name])
+        self.__modules = modules
 
 
 T = TypeVar('T')
 
 
 def _dict_to_list(dct: Dict) -> List[T]:
-    return [dct.keys()]
+    return list(dct.keys())[0:]
 
 
 def _list_to_dict(lst: List[T]) -> Dict[str, T]:
