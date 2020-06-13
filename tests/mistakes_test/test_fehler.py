4fec741f3c60db13f68dec8354cc0e7125894432
Lars Reimann <mail@larsreimann.com>
2020-06-13 20:44:09 +0200

    Fix a test class

--- tests/mistakes_test/test_fehler.py
+++ tests/mistakes_test/test_fehler.py
@@ -7,9 +7,10 @@ from analyses.messages._message import Message
 class TestFehlerManager(TestCase):
     def test_print_help(self):
         f1 = Message("Datei", 2, "Beschreibung")
-        result = "error occured  in [Pfad in file Datei] in line 2: Beschreibung"
+        result = "Error in 'Datei' line 2: Beschreibung"
 
         self.assertEqual(str(f1), result)
+
 
 if __name__ == '__main__':
     unittest.main()
