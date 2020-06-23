40c8053522c9b6cf6335e184a3af2f5482158494
Eldeib <mohammed.eldeib@gmail.com>
2020-06-23 12:42:42 +0200

    the task is finished, and it's being tested

--- src/code_analyzer.py
+++ src/code_analyzer.py
@@ -26,9 +26,9 @@ def analyze_file(file_to_analyze: str, package: Package, message_manager: Messag
     calls = parse_function_calls(file_to_analyze, package)
 
     for call in calls:
-        check_function_exists(call, message_manager)
-        check_arg_names(call, message_manager)
-        check_arg_number(call, message_manager)
+        # check_function_exists(call, message_manager)
+        # check_arg_names(call, message_manager)
+        # check_arg_number(call, message_manager)
         check_type(call, message_manager)
     return message_manager
 
