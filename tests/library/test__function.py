c13f0b3d435a25892af8d6d17bc470cce7a37237
hodzy <hadyelkady1996@gmail.com>
2020-06-23 14:47:27 +0200

    parameter Klasse getestet

---
+++ tests/library/test__function.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestFunction(TestCase):
+    def test_get_name(self):
+        self.fail()
+
+    def test_get_parameter(self):
+        self.fail()
+
+    def test_get_parameters(self):
+        self.fail()
---
+++ tests/library/test__klass.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestClass(TestCase):
+    def test_get_name(self):
+        self.fail()
+
+    def test_add_method(self):
+        self.fail()
+
+    def test_get_methods_with_name(self):
+        self.fail()
+
+    def test_get_all_methods(self):
+        self.fail()
